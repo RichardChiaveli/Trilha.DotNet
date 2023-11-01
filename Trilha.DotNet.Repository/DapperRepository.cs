b468b78fa497885144bad19cdb96a2e0e714356e
Richard Cleyton Chiaveli <richard.chiavelli@linx.com.br>
2023-11-01 17:18:37 -0300

    Correção IDataReader no Repository

--- Trilha.DotNet.Repository/DapperRepository.cs
+++ Trilha.DotNet.Repository/DapperRepository.cs
@@ -26,13 +26,18 @@ public abstract class DapperRepository
         return await conn.QueryAsync<T>(query, arguments, transaction, 0, commandType);
     }
 
-    public async Task<IDataReader> GetBy(string query
+    public async Task<IEnumerable<DataRow>> GetBy(string query
         , object? arguments = null
         , CommandType commandType = CommandType.Text
         , IDbTransaction? transaction = null)
     {
         using var conn = GetDbConnection();
-        return await conn.ExecuteReaderAsync(query, arguments, transaction, 0, commandType);
+        var result = await conn.ExecuteReaderAsync(query, arguments, transaction, 0, commandType);
+
+        var dt = new DataTable();
+        dt.Load(result);
+
+        return from DataRow row in dt.Rows select row;
     }
 
     public async Task<bool> AddUpdateOrDelete(string query
