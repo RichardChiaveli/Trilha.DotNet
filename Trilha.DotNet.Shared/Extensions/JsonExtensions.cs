03387582c906aa258ceb25a22696432a34bb51c7
Richard Cleyton Chiaveli <richard.chiavelli@linx.com.br>
2024-01-11 18:59:15 -0300

    Newtonsoft para System.Text.Json

--- Trilha.DotNet.Shared/Extensions/JsonExtensions.cs
+++ Trilha.DotNet.Shared/Extensions/JsonExtensions.cs
@@ -2,28 +2,17 @@
 
 public static class JsonExtensions
 {
-    public static JsonSerializerSettings AddDefaultSettings(this JsonSerializerSettings settings)
+    public static readonly JsonSerializerOptions DefaultSettings = new()
     {
-        settings.Formatting = Formatting.None;
-        settings.NullValueHandling = NullValueHandling.Ignore;
-        settings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
-        settings.DefaultValueHandling = DefaultValueHandling.Include;
-        settings.ContractResolver = new CamelCasePropertyNamesContractResolver();
-
-        return settings;
-    }
+        DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull,
+        ReferenceHandler = ReferenceHandler.IgnoreCycles,
+        PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+        WriteIndented = false
+    };
 
     public static string Stringify(this object obj)
-        => JsonConvert.SerializeObject(obj, new JsonSerializerSettings().AddDefaultSettings());
+        => JsonSerializer.Serialize(obj, DefaultSettings);
 
     public static T ParseJson<T>(this string json)
-        => JsonConvert.DeserializeObject<T>(json, new JsonSerializerSettings().AddDefaultSettings())!;
-
-    public static T GetValue<T>(this JObject param, string key) where T : IConvertible
-    {
-        var jToken = param.GetValue(key);
-        var obj = Convert.ChangeType(jToken, typeof(T));
-
-        return (T)obj!;
-    }
+        => JsonSerializer.Deserialize<T>(json, DefaultSettings)!;
 }
